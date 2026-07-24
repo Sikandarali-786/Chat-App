2be695781f0b392ece3c3257c936cd417139ff49
Sikandarali-786 <sikandarjutt1717@gmail.com>
2026-07-24 18:10:58 +0500

    feat(config): add environment configuration

---
+++ server/src/shared/config/env.ts
@@ -0,0 +1,23 @@
+import { cleanEnv, port, str } from "envalid";
+
+export const env = cleanEnv(process.env, {
+    NODE_ENV: str({
+        choices: ["development", "production", "test"],
+    }),
+
+    PORT: port(),
+
+    API_PREFIX: str(),
+
+    MONGODB_URI: str(),
+
+    JWT_ACCESS_SECRET: str(),
+
+    JWT_REFRESH_SECRET: str(),
+
+    ACCESS_TOKEN_EXPIRES_IN: str(),
+
+    REFRESH_TOKEN_EXPIRES_IN: str(),
+
+    CLIENT_URL: str(),
+});
